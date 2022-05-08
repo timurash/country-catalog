f945ee26b98024b0edbe8743ed91b6590443bd0a
Timur Akhmetov <akhmetov.t.i@gmail.com>
2022-05-08 20:12:50 +0300

    feat(front): ESLint config update #3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,31 @@
 module.exports = {
   env: {
     node: true,
+    'vue/setup-compiler-macros': true,
   },
-  extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-  ],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/script-setup-uses-vars': 'error',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.vue'],
+      parser: 'vue-eslint-parser',
+      rules: {
+      },
+      extends: [
+        'airbnb-base',
+        'plugin:vue/vue3-recommended',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: ['./tsconfig.json'],
+        extraFileExtensions: ['.vue'],
+      },
+    },
+  ],
 };
