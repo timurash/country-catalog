28804dfcd13c86336968bec551f0879d9c2e0b3f
Timur Akhmetov <akhmetov.t.i@gmail.com>
2022-05-09 21:00:11 +0300

    feat(front): Added vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
+import Router from './router';
 
 const app = createApp(App);
 
-app.use(ElementPlus);
+app.use(Router);
 app.mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const CountryCatalog = () => import('../components/CountryCatalog/CountryCatalog.vue');
+const CountryPage = () => import('../components/CountryPage/CountryPage.vue');
+const AboutPage = () => import('../components/AboutPage/AboutPage.vue');
+
+const routes = [
+  { path: '/', component: CountryCatalog },
+  { path: '/country/:countryName', component: CountryPage },
+  { path: '/about', component: AboutPage },
+];
+
+export default createRouter({
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+  history: createWebHistory(),
+  routes,
+});
