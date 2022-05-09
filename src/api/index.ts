f9fee883e1767336db9e4b645074a56151829b84
Timur Akhmetov <akhmetov.t.i@gmail.com>
2022-05-09 22:14:19 +0300

    feat(front): Optimize API calls

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,36 @@
 import CountryModel from '../models/CountryModel/CountryModel';
 
+const allCountriesQueryFields = [
+  'name',
+  'capital',
+  'currencies',
+  'languages',
+  'flags',
+];
+
+const countriesByNameQueryFields = [
+  'name',
+  'capital',
+  'currencies',
+  'languages',
+  'population',
+  'flags',
+];
+
 export async function getAllCountries(): Promise<Array<CountryModel>> {
-  return fetch('https://restcountries.com/v3.1/name/rus')
+  const url = new URL('https://restcountries.com/v3.1/all');
+  url.searchParams.append('fields', allCountriesQueryFields.join(','));
+
+  return fetch(url.toString())
     .then((response) => response.json()) as Promise<Array<CountryModel>>;
 }
 
 export async function getCountriesByName(searchText: string):
 Promise<Array<CountryModel>> {
-  return fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+  const url = new URL(`https://restcountries.com/v3.1/name/${searchText}`);
+  url.searchParams.append('fields', countriesByNameQueryFields.join(','));
+
+  return fetch(url.toString())
     .then((response) => response.json()) as Promise<Array<CountryModel>>;
 }
 
